1934036d58e35331ac87f14fffc5f13247c98769
mohisinabbas <mohisinabbas@users.noreply.github.com>
2020-10-11 17:07:52 -0400

    first commit

---
+++ src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,74 @@
+package com.example.user.controller;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.user.model.User;
+import com.example.user.repository.UserRepository;
+import com.example.user.service.UserService;
+
+@RestController
+public class UserController {
+
+	@Autowired
+	public UserService userService;
+
+	@GetMapping(path = "/user/{id}")
+	public Optional<User> getUserById(@PathVariable int id) { 
+			return userService.getUserById(id);
+	}
+	
+	@GetMapping(path = "/userByPhone")
+	public Optional<User> getUserByPhone(@RequestParam(value = "phone", required = true) String phone) {
+		return userService.getUserByPhone(phone);
+	}
+
+	@GetMapping(path = "/user/all")
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
+	}
+
+	@PostMapping(path = "/user")
+	public String addUser(@RequestBody User user) {
+		System.out.println(user);
+		return userService.addUser(user);
+	}
+
+	@PutMapping(path = "/user/{userId}")
+	public String updateUser(@PathVariable int userId, @RequestBody User user) throws Exception {
+		// Optional<Optional<User>> op = Optional.of(userService.getUserById(userId));
+		if (userService.getUserById(userId).isPresent()) {
+			User checkUser = userService.getUserById(userId).get();
+			checkUser.setFirstName(user.getFirstName());
+			checkUser.setMiddleName(user.getMiddleName());
+			checkUser.setLastName(user.getLastName());
+			checkUser.setPhone(user.getPhone());	
+			return userService.updateUser(checkUser);
+		}else {
+			return "User Not Found";
+		}
+	}
+
+	@DeleteMapping(path = "/user/{userId}")
+	public String deleteUser(@PathVariable int userId) {
+		if (userService.getUserById(userId).isPresent()) {
+			userService.deleteUser(userId);
+			return "Deleted succesfully";
+		}else {
+			return "User Not Found";
+		}
+		
+	}
+
+}
---
+++ src/main/java/com/example/user/model/Address.java
@@ -0,0 +1,65 @@
+package com.example.user.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "address")
+public class Address {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String city;
+	private String state;
+	private int zipCode;
+
+	public Address() {
+
+	}
+	
+	public Address(int id, String city, String state, int zipCode) {
+		super();
+		this.id = id;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public int getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(int zipCode) {
+		this.zipCode = zipCode;
+	}
+
+}
